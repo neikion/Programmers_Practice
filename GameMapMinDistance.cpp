eef507b9f0db01e90ddff1781deacbba63ac3190
neikion <neikion43@gmail.com>
2023-09-21 21:54:13 +0900

    Add GameMapMinDistance

--- GameMapMinDistance.cpp
+++ GameMapMinDistance.cpp
@@ -2,51 +2,59 @@
 
 #include<vector>
 #include<algorithm>
-#include<iostream>
+#include<queue>
 
 using namespace std;
 
 int total;
+class node {
+public:
+    int x;
+    int y;
+    node() = default;
+    node(int ix, int iy) :x(ix), y(iy) {
 
-void search(vector<vector<int>>& maps, vector<vector<int>> path, int target[2], int pathcount) {
-    path[target[0]][target[1]] = 0;
-    pathcount++;
-    if (target[0] == maps.size() - 1 && target[1] == maps[maps.size() - 1].size() - 1) {
-        if (total > pathcount) {
-            total = pathcount;
-        }
     }
+};
+int search(vector<vector<int>>& maps, vector<vector<int>> path) {
+    path[0][0] = 1;
     int pos[2];
-    for (int i = -1; i < 2; i += 2) {
-        for (int j = 0; j < 2; j++) {
-            if (j == 0) {
-                pos[0] = target[0] + i;
-                pos[1] = target[1];
+    queue<node> waiting;
+    waiting.push(node(0, 0));
+    node targetnode, value;
+    while (waiting.size() > 0) {
+        for (int i = -1; i < 2; i += 2) {
+            value = waiting.front();
+            for (int j = 0; j < 2; j++) {
+                if (j == 0) {
+                    targetnode.x = value.x + i;
+                    targetnode.y = value.y;
+                }
+                else {
+                    targetnode.x = value.x;
+                    targetnode.y = value.y + i;
+                }
+                if (targetnode.x < 0 || targetnode.y < 0) {
+                    continue;
+                }
+                if (targetnode.x >= maps.size() || targetnode.y >= maps[targetnode.x].size()) {
+                    continue;
+                }
+                if (maps[targetnode.x][targetnode.y] == 1 && path[targetnode.x][targetnode.y] + 1 < path[value.x][value.y]) {
+                    path[targetnode.x][targetnode.y] = path[value.x][value.y] + 1;
+                    if (targetnode.x == maps.size() - 1 && targetnode.y == maps[maps.size() - 1].size() - 1) {
+                        return path[maps.size() - 1][maps[maps.size() - 1].size() - 1];
+                    }
+                    waiting.push(node(targetnode.x, targetnode.y));
+                }
             }
-            else {
-                pos[0] = target[0];
-                pos[1] = target[1] + i;
-            }
-            if (pos[0] < 0 || pos[1] < 0) {
-                continue;
-            }
-            if (pos[0] > maps.size() - 1 || pos[1] > maps[pos[0]].size() - 1) {
-                continue;
-            }
-            if (path[pos[0]][pos[1]] == 0) {
-                continue;
-            }
-            search(maps, path, pos, pathcount);
         }
+        waiting.pop();
     }
+    return path[maps.size() - 1][maps[maps.size() - 1].size() - 1];
 }
+
 int solution(vector<vector<int> > maps)
 {
-    int mypos[2] = { 0,0 };
-    total = (maps.size() * maps[maps.size() - 1].size()) + 1;
-    search(maps, maps, mypos, 0);
-    if (total == (maps.size() * maps[maps.size() - 1].size()) + 1) {
-        total = -1;
-    }
-    return total;
+    return search(maps, vector<vector<int>>(maps.size(), vector<int>(maps[0].size(), -1)));
 }
