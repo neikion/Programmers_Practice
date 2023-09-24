7a4116234f1ac893a6533cfb71e0606027495a6e
neikion <neikion43@gmail.com>
2023-09-24 22:41:25 +0900

    Add WordChange

--- WordChange.cpp
+++ WordChange.cpp
@@ -1,21 +1,14 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/43163
+
 #include <string>
 #include <vector>
 #include<algorithm>
-#include<iostream>
 using namespace std;
-bool checkAnswer(string& a, string& b) {
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
 
 bool overlapDouble(string& begin, string& check, string& target) {
     bool unique = false;
     for (int i = 0; i < begin.size(); i++) {
-        if (begin[i] != check[i] && check[i] == target[i]) {
+        if (begin[i] == check[i]) {
             if (!unique) {
                 unique = true;
             }
@@ -27,48 +20,63 @@ bool overlapDouble(string& begin, string& check, string& target) {
     return false;
 }
 
-
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
     string check;
+    bool checked = true;
+    int ibefore = 99;
+    string sbefore;
     if (find(words.begin(), words.end(), target) == words.end()) {
         return 0;
     }
-    for (int i = 0; i < words.size(); i++) {
-        check = words[i];
-        if (check.size() < 3 || check.size() > 10) {
-            continue;
-        }
-        if (overlapDouble(begin, check, target)) {
-            continue;
-        }
-        for (int j = 0; j < check.size(); j++) {
-            if (begin[j] != check[j] && begin[j] != target[j]) {
-                int itarget = (int)target[j];
-                int ibegin = (int)begin[j];
-                int icheck = (int)check[j];
-                int stand, checkclac;
-                if (ibegin > itarget) {
-                    stand = ibegin - itarget;
-                }
-                else {
-                    stand = itarget - ibegin;
-                }
-                if (icheck > itarget) {
-                    checkclac = icheck - itarget;
-                }
-                else {
-                    checkclac = itarget - icheck;
-                }
-                cout << begin << " " << check << endl;
-                if (checkclac <= stand) {
-                    begin[j] = check[j];
-                    answer++;
+    while (checked) {
+        checked = false;
+        for (int i = 0; i < words.size(); i++) {
+            check = words[i];
+            if (check.size() < 3 || check.size() > 10) {
+                continue;
+            }
+            if (!overlapDouble(begin, check, target)) {
+                continue;
+            }
+            if (begin.compare(check) == 0) {
+                continue;
+            }
+            for (int j = 0; j < check.size(); j++) {
+                if (begin[j] != check[j] && begin[j] != target[j]) {
+                    int itarget = (int)target[j];
+                    int ibegin = (int)begin[j];
+                    int icheck = (int)check[j];
+                    int stand, checkclac;
+                    if (ibegin > itarget) {
+                        stand = ibegin - itarget;
+                    }
+                    else {
+                        stand = itarget - ibegin;
+                    }
+                    if (icheck > itarget) {
+                        checkclac = icheck - itarget;
+                    }
+                    else {
+                        checkclac = itarget - icheck;
+                    }
+                    if (checkclac <= stand) {
+                        if (checkclac < ibefore) {
+                            ibefore = checkclac;
+                            sbefore = check;
+                        }
+                    }
                 }
             }
-        }
-        if (checkAnswer(begin, target)) {
-            return answer;
+            if (ibefore != 99) {
+                begin = sbefore;
+                answer++;
+                ibefore = 99;
+                checked = true;
+            }
+            if (begin.compare(target)==0) {
+                return answer;
+            }
         }
     }
     return answer;
