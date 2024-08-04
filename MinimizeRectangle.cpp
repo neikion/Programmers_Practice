d1371ee8ecddfdb54a43a60540d622cd372b6654
neikion <neikion43@gmail.com>
2024-08-05 08:34:30 +0900

    update MnimizeRectangle
    add namespace for avoid crash

--- MinimizeRectangle.cpp
+++ MinimizeRectangle.cpp
@@ -3,21 +3,22 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-int solution(vector<vector<int>> sizes) {
-    int answer = 0;
-    int maxRow = 0;
-    int maxCol = 0;
-    for (int i = 0; i < sizes.size(); i++) {
-        if (sizes[i][0] < sizes[i][1]) {
-            int temp = sizes[i][0];
-            sizes[i][0] = sizes[i][1];
-            sizes[i][1] = temp;
+namespace MinimizeRectangle {
+    using namespace std;
+    int solution(vector<vector<int>> sizes) {
+        int answer = 0;
+        int maxRow = 0;
+        int maxCol = 0;
+        for (int i = 0; i < sizes.size(); i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+            if (sizes[i][0] > maxRow) maxRow = sizes[i][0];
+            if (sizes[i][0] > maxCol) maxCol = sizes[i][1];
         }
-        if (sizes[i][0] > maxRow) maxRow = sizes[i][0];
-        if (sizes[i][0] > maxCol) maxCol = sizes[i][1];
+        answer = maxRow * maxCol;
+        return answer;
     }
-    answer = maxRow * maxCol;
-    return answer;
 }
