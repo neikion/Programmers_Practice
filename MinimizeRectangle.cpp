1cb0cd6e6657cf9f15aac242e320cbc7a6c99be3
neikion <neikion43@gmail.com>
2024-08-05 06:06:25 +0900

    Add MinimizeRectangle

---
+++ MinimizeRectangle.cpp
@@ -0,0 +1,23 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/86491
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    int maxRow = 0;
+    int maxCol = 0;
+    for (int i = 0; i < sizes.size(); i++) {
+        if (sizes[i][0] < sizes[i][1]) {
+            int temp = sizes[i][0];
+            sizes[i][0] = sizes[i][1];
+            sizes[i][1] = temp;
+        }
+        if (sizes[i][0] > maxRow) maxRow = sizes[i][0];
+        if (sizes[i][0] > maxCol) maxCol = sizes[i][1];
+    }
+    answer = maxRow * maxCol;
+    return answer;
+}
