45c01fd14ab7b92a3347fe41b9eb48831f0b4b10
neikion <neikion43@gmail.com>
2024-12-20 00:50:02 +0900

    update KNumber
    add namespace

--- KNumber.cpp
+++ KNumber.cpp
@@ -3,14 +3,17 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer = vector<int>(), result;
-    for (int i = 0; i < commands.size(); i++) {
-        result = vector<int>(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
-        sort(result.begin(), result.end());
-        answer.push_back(result[commands[i][2] - 1]);
+namespace Knumber {
+    using namespace std;
+    vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+        vector<int> answer = vector<int>(), result;
+        for (int i = 0; i < commands.size(); i++) {
+            result = vector<int>(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
+            sort(result.begin(), result.end());
+            answer.push_back(result[commands[i][2] - 1]);
+        }
+        return answer;
     }
-    return answer;
 }
+
